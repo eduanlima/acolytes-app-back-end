7e72955865c87a1b051abf0835e5d2b08d42c0d3
eduanlima@github.com <eduandev@gmail.com>
2025-05-25 19:19:37 -0300

    Project created

---
+++ src/main/java/br/com/posterius/acolyte_app/AcolyteAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.posterius.acolyte_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcolyteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcolyteAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/posterius/acolyte_app/AcolyteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.posterius.acolyte_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcolyteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
