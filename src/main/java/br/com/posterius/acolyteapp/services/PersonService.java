ef81ad950673ab1a6f4f4cfe50b328b52f09ed6c
eduanlima@github.com <eduandev@gmail.com>
2025-06-04 23:07:54 -0300

    End function to generate code by person and insert to person

--- src/main/java/br/com/posterius/acolyteapp/services/PersonService.java
+++ src/main/java/br/com/posterius/acolyteapp/services/PersonService.java
@@ -2,11 +2,13 @@ package br.com.posterius.acolyteapp.services;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.posterius.acolyteapp.dto.PersonDTO;
+import br.com.posterius.acolyteapp.entities.Person;
 import br.com.posterius.acolyteapp.repositories.PersonRepository;
 
 @Service
@@ -17,5 +19,19 @@ public class PersonService {
 	@Transactional(readOnly = true)
 	public List<PersonDTO> findAll(){
 		return personRepository.findAll().stream().map(e -> new PersonDTO(e)).toList();
+	}
+	
+	@Transactional
+	public PersonDTO insert(PersonDTO dto) {
+		Person entity = new Person();
+		copyDtoToEntity(dto, entity);
+		Integer lastCode = personRepository.findMaxCode();
+		entity.setCode(lastCode == null ? 1 : lastCode + 1);
+		entity = personRepository.save(entity);
+		return new PersonDTO(entity);
+	}
+	
+	private void copyDtoToEntity(PersonDTO dto, Person entity ) {
+		BeanUtils.copyProperties(dto, entity);
 	}
 }
