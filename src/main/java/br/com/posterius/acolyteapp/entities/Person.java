99f0b567c780fe741d4da495ef75d39eeaadc3d8
eduanlima@github.com <eduandev@gmail.com>
2025-06-07 23:47:26 -0300

    Constraints in person model

--- src/main/java/br/com/posterius/acolyteapp/entities/Person.java
+++ src/main/java/br/com/posterius/acolyteapp/entities/Person.java
@@ -3,11 +3,13 @@ package br.com.posterius.acolyteapp.entities;
 import java.sql.Timestamp;
 import java.util.UUID;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -21,8 +23,14 @@ public class Person {
 	@Id
 	@GeneratedValue(generator = "UUID")
 	private UUID id;
+	@NotNull
 	private Integer code;
+	@NotNull
+	@NotBlank
 	private String firstName;
+	@NotNull
+	@NotBlank
 	private String lastName;
+	@NotNull
 	private Timestamp dateBirth;
 }
