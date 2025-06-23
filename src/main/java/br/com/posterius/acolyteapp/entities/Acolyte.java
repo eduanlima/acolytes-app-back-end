a15009711d3a2b3ad4094f38db090c0d86893a52
eduanlima@github.com <eduandev@gmail.com>
2025-06-22 22:26:12 -0300

    Created acolyte model

---
+++ src/main/java/br/com/posterius/acolyteapp/entities/Acolyte.java
@@ -0,0 +1,14 @@
+package br.com.posterius.acolyteapp.entities;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Acolyte {
+	private UUID id;
+	private Person person;
+	private AcolyteType acolyteType;
+}
