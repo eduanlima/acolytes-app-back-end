89669bbcb3992eb6a2c9456d820fc196d457cc93
eduanlima@github.com <eduandev@gmail.com>
2025-06-26 22:57:37 -0300

    Created constructor without args

--- src/main/java/br/com/posterius/acolyteapp/entities/Position.java
+++ src/main/java/br/com/posterius/acolyteapp/entities/Position.java
@@ -3,7 +3,9 @@ package br.com.posterius.acolyteapp.entities;
 import java.util.UUID;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Data
 public class Position {
 	private UUID id;
