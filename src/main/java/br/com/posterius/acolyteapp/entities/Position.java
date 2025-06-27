d4ddf18e9bd00bae306eee6882dfa7de3245b522
eduanlima@github.com <eduandev@gmail.com>
2025-06-27 15:09:14 -0300

    Add method

--- src/main/java/br/com/posterius/acolyteapp/entities/Position.java
+++ src/main/java/br/com/posterius/acolyteapp/entities/Position.java
@@ -2,10 +2,12 @@ package br.com.posterius.acolyteapp.entities;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Position {
 	private UUID id;
