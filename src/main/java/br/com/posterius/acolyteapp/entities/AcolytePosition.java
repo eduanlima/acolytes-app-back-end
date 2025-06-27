9f659d375f13ef70a23537a023138595ba94f79f
eduanlima@github.com <eduandev@gmail.com>
2025-06-27 15:13:12 -0300

    All args constructor into model

--- src/main/java/br/com/posterius/acolyteapp/entities/AcolytePosition.java
+++ src/main/java/br/com/posterius/acolyteapp/entities/AcolytePosition.java
@@ -2,8 +2,10 @@ package br.com.posterius.acolyteapp.entities;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
 public class AcolytePosition {
 	private UUID id;
