2083a578587964bf2d2b74118c1eaf85626e76e5
eduanlima@github.com <eduandev@gmail.com>
2025-06-28 23:01:14 -0300

    Hascode and equals acolyte

--- src/main/java/br/com/posterius/acolyteapp/entities/Acolyte.java
+++ src/main/java/br/com/posterius/acolyteapp/entities/Acolyte.java
@@ -4,10 +4,12 @@ import java.util.UUID;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @AllArgsConstructor
 @Data
 public class Acolyte {
+	@EqualsAndHashCode.Include
 	private UUID id;
 	private Person person;
 }
