fcd57ab06fd75b199675c59c3d2f0b87cd3d526a
eduanlima@github.com <eduandev@gmail.com>
2025-06-28 23:00:07 -0300

    Hascode and equals acolyte position

--- src/main/java/br/com/posterius/acolyteapp/entities/AcolytePosition.java
+++ src/main/java/br/com/posterius/acolyteapp/entities/AcolytePosition.java
@@ -4,10 +4,12 @@ import java.util.UUID;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @AllArgsConstructor
 @Data
 public class AcolytePosition {
+	@EqualsAndHashCode.Include
 	private UUID id;
 	private Acolyte acolyte;
 	private Position position;
